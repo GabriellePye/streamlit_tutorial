606bd6bc41d3315a3501bca3e45f8ce760a59a92
GabriellePye <gabriellepye@rockborne.com>
2024-11-21 10:49:10 +0000

    - added app

---
+++ pawsitivity.py
@@ -0,0 +1,24 @@
+#-- Pawsitivity Streamlit App --#
+
+# -------------------------
+# Libraries & Functions
+# -------------------------
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# API 
+
+# -------------------------
+# 1. Background CSS
+# -------------------------
+
+st.markdown("""
+<style>
+/* background */
+.stApp {
+    background: url('https://thumbs.dreamstime.com/b/dog-paw-seamless-pattern-vector-cat-paw-foot-print-isolated-wallpaper-tile-background-backdrop-brown-dog-paw-seamless-pattern-124895915.jpg');
+    background-size: cover; 
+    background-position: top;
+}
